06607d7c4ffe3fd80686a59ffe8c5f30fe85b51f
mayaaio <mayaitkin-ofer@maya.itkin-ofer>
2023-08-13 19:24:55 -0700

    add padding to top

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,30 +1,34 @@
-import { ActionIcon, AppShell, Container, Group, Paper } from '@mantine/core';
-import { GitHub, Home } from 'react-feather';
+import { ActionIcon, AppShell, Container, Group, Paper } from "@mantine/core";
+import { GitHub, Home } from "react-feather";
 import { Link, Outlet } from "react-router-dom";
-import LightDarkButton from '../components/LightDarkButton';
+import LightDarkButton from "../components/LightDarkButton";
 
 function Layout() {
-    return(
-        <Paper padding='md' radius={0} style={{minHeight: '100vh'}}>
-        <AppShell 
-        header={
-            <Group mx='md'>
-                <ActionIcon component={Link} to="/">
-                    <Home />
-                </ActionIcon>
-                <ActionIcon component={Link} target='_blank' to="https://github.com/mayaaio">
-                   <GitHub />
-                </ActionIcon>
-                <LightDarkButton />
-            </Group>
-        }
-        >
-            <Container size={420} my={40}>
-                <Outlet />
-            </Container>
-        </AppShell>
-        </Paper>
-    )
+	return (
+		<Paper padding="md" radius={0} style={{ minHeight: "100vh" }}>
+			<AppShell
+				header={
+					<Group px="md" py="md">
+						<ActionIcon component={Link} to="/">
+							<Home />
+						</ActionIcon>
+						<ActionIcon
+							component={Link}
+							target="_blank"
+							to="https://github.com/mayaaio"
+						>
+							<GitHub />
+						</ActionIcon>
+						<LightDarkButton />
+					</Group>
+				}
+			>
+				<Container size={420} my={40}>
+					<Outlet />
+				</Container>
+			</AppShell>
+		</Paper>
+	);
 }
 
-export default Layout
+export default Layout;
