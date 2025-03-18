dcc87a63a68501a8e3348cbf9d55e862ba725a2b
mayaaio <itkin.maya@gmail.com>
2025-03-18 18:14:14 +0200

    calling photos from cloudinary

--- src/components/PhotoCarousel.tsx
+++ src/components/PhotoCarousel.tsx
@@ -1,7 +1,6 @@
 import { Carousel } from "@mantine/carousel";
 import { useEffect, useState } from "react";
 import PhotoCard from "./PhotoCard";
-import { Container } from "@mantine/core";
 
 function PhotoCarousel() {
   const [images, setImages] = useState([]);
@@ -10,9 +9,30 @@ function PhotoCarousel() {
     const fetchImagesData = async () => {
       try {
         // Fetch the image metadata JSON file
-        const response = await fetch("/images.json");
+        //const response = await fetch("/images.json");
+        const response = await fetch(
+          `https://res.cloudinary.com/` +
+            process.env.REACT_APP_CLOUDINARY_CLOUD_NAME +
+            `/image/list/portfolio.json`
+        );
         const data = await response.json();
-        setImages(data); // Set the image metadata to state
+        console.log(data);
+
+        const imageData = data.resources || [];
+
+        if (Array.isArray(imageData)) {
+          const filteredImages = imageData.map((image) => ({
+            public_id: image.public_id,
+            format: image.format,
+            ...(image.context?.custom?.Location
+              ? { location: image.context.custom.Location }
+              : {}),
+          }));
+
+          setImages(filteredImages); // Set state with the filtered images
+        } else {
+          console.error("Expected an array of images but got:", imageData);
+        }
       } catch (error) {
         console.error("Error fetching image metadata:", error);
       }
@@ -29,7 +49,7 @@ function PhotoCarousel() {
       controlsOffset="sm"
       loop
       dragFree
-      slidesToScroll={3}
+      // slidesToScroll={3} // update this later to reflect the size of the viewport
     >
       {images.map((image, index) => (
         <Carousel.Slide key={index}>
