f6149bfe2dde267f65546f95807aef19333a3d58
mayaaio <itkin.maya@gmail.com>
2025-03-18 18:42:12 +0200

    underline linkedin, start to work on contact form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,20 @@
-import { Title, Text } from "@mantine/core"
-import { Link } from "react-router-dom"
+import { Title, Text } from "@mantine/core";
+import { Link } from "react-router-dom";
 
 function Contact() {
-    return(
-        <div>
-            <Title>Contact</Title>
-            <Text component={Link} target="_blank" to="https://www.linkedin.com/in/mayaitkinofer/">Linkedin</Text>
-        </div>
-    )
+  return (
+    <div>
+      <Title>Contact</Title>
+      <Text
+        component={Link}
+        target="_blank"
+        to="https://www.linkedin.com/in/mayaitkinofer/"
+        td="underline"
+      >
+        Add me on Linkedin
+      </Text>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
